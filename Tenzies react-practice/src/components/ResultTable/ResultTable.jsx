b4b16a993baab5cdc5f8c9f6230bb1600794fe01
gilotin <toshevnikolay@gmail.com>
2025-03-07 07:23:15 +0200

    fix: typo

--- Tenzies react-practice/src/components/ResultTable/ResultTable.jsx
+++ Tenzies react-practice/src/components/ResultTable/ResultTable.jsx
@@ -3,7 +3,7 @@ function Result(props) {
         <section className="result__table">
             <h2 className="result__header">Congratulation!!!</h2>
             <p className="result__description">
-                You wont the game with only <span>{props.rollCounter}</span> rolls!!!
+                You won the game with only <span>{props.rollCounter}</span> rolls!!!
             </p>
         </section>
     );
